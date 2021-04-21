330d073d66983d3f19db82ef82670cfa7acb0a14
Abi <abi@alterra.id>
2021-04-22 06:15:15 +0700

    Release action

---
+++ __tests__/environment.test.ts
@@ -0,0 +1,11 @@
+import {deleteEnvironment} from '../src/environment'
+
+test('Delete invalid environment', async () => {
+  const key = 'JIRA-404'
+  await expect(deleteEnvironment('', key)).resolves.toEqual(404)
+})
+
+test('Delete environment', async () => {
+  const key = 'JIRA-204'
+  await expect(deleteEnvironment('', key)).resolves.toEqual(204)
+})
