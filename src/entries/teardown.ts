a407854803863c5da6f882b4b800cb0e892d891f
Abi <abi@alterra.id>
2021-05-11 19:40:05 +0700

    Move teardown to post action

---
+++ src/entries/teardown.ts
@@ -0,0 +1,35 @@
+import * as core from '@actions/core'
+import { exec } from '@actions/exec'
+import yn from 'yn'
+import deleteEnvironment from '../environment'
+import State from '../state'
+
+async function run(): Promise<void> {
+  try {
+    const key = core.getState(State.KEY)
+
+    const push = yn(core.getInput('push', { required: true }))
+    if (push) {
+      const prereleaseTag = core.getState(State.PRERELEASE_TAG)
+      const releaseTag = core.getState(State.RELEASE_TAG)
+
+      await core.group('Create release tag', async () => {
+        await exec('git', ['tag', releaseTag, key])
+        await exec('git', ['push', 'origin', releaseTag])
+      })
+
+      await core.group('Clean up prerelease tag', async () => {
+        await exec('git', ['tag', '--delete', prereleaseTag, key])
+        await exec('git', ['push', '--delete', 'origin', key])
+        await exec('git', ['push', '--delete', 'origin', prereleaseTag])
+      })
+    }
+
+    const token = core.getInput('token', { required: true })
+    await deleteEnvironment(token, key)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
--- src/release.ts
+++ src/release.ts
@@ -1,37 +1,21 @@
 import * as core from '@actions/core'
-import { exec } from '@actions/exec'
-import yn from 'yn'
-import deleteEnvironment from './environment'
+import State from './state'
 import { prereleaseTagFromKey, releaseTagFromPrerelease } from './tag'
 
 async function run(): Promise<void> {
   try {
     const key = core.getInput('key', { required: true }).toUpperCase()
-    const token = core.getInput('token', { required: true })
-
     const prereleaseTag = await prereleaseTagFromKey(key)
     if (prereleaseTag === undefined) {
       core.setFailed("Can't find prerelease tag")
       return
     }
-
     const releaseTag = releaseTagFromPrerelease(prereleaseTag)
 
-    const push = yn(core.getInput('push', { required: true }))
-    if (push) {
-      await core.group('Create release tag', async () => {
-        await exec('git', ['tag', releaseTag, key])
-        await exec('git', ['push', 'origin', releaseTag])
-      })
+    core.saveState(State.KEY, key)
+    core.saveState(State.PRERELEASE_TAG, prereleaseTag)
+    core.saveState(State.RELEASE_TAG, releaseTag)
 
-      await core.group('Clean up prerelease tag', async () => {
-        await exec('git', ['tag', '--delete', prereleaseTag, key])
-        await exec('git', ['push', '--delete', 'origin', key])
-        await exec('git', ['push', '--delete', 'origin', prereleaseTag])
-      })
-    }
-
-    await deleteEnvironment(token, key)
     core.setOutput('tag', releaseTag)
   } catch (error) {
     core.setFailed(error.message)
